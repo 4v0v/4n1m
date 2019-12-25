a32ec66ccee57b891173d33df55ee7e2755b0da2
4v0v <4vZEROv@gmail.com>
2019-12-25 14:08:22 +0100

    window pos

--- main.cpp
+++ main.cpp
@@ -4,16 +4,8 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     MainWindow window;
-    //QDesktopWidget *desk = new QDesktopWidget();
-//    window.setGeometry(
-//        QStyle::alignedRect(
-//            Qt::LeftToRight,
-//            Qt::AlignCenter,
-//            window.size(),
-//            desk->screenGeometry(1)
-//        )
-//    );
-    window.setGeometry(50, 50, 850, 650);
+
+    window.setGeometry(100, 100, 850, 650);
     window.show();
 
     return app.exec();
